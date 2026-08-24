9239e83d23009430f46d2a4e1c43e97a0c32d333
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-08-24 17:46:26 -0300

    feat: direction custom type

--- 2025/1/Main.hs
+++ 2025/1/Main.hs
@@ -9,35 +9,43 @@ main = do
     print part1
     print part2
 
-parseInstruction :: String -> (Char, Int)
-parseInstruction ('L' : amount) = ('L', read amount)
-parseInstruction ('R' : amount) = ('R', read amount)
+data Direction = L | R
+    deriving (Show)
 
-movePart1 :: (Int, Int) -> (Char, Int) -> (Int, Int)
-movePart1 (position, count) (direction, amount) =
-    let movement =
-            if direction == 'L'
-                then -amount
-                else amount
-        newPosition = (position + movement) `mod` 100
+parseInstruction :: String -> (Direction, Int)
+parseInstruction ('L' : amount) = (L, read amount)
+parseInstruction ('R' : amount) = (R, read amount)
+
+movePart1 :: (Int, Int) -> (Direction, Int) -> (Int, Int)
+movePart1 (position, count) (L, amount) =
+    let newPosition = (position - amount) `mod` 100
+        newCount =
+            if newPosition == 0
+                then count + 1
+                else count
+     in (newPosition, newCount)
+movePart1 (position, count) (R, amount) =
+    let newPosition = (position + amount) `mod` 100
         newCount =
             if newPosition == 0
                 then count + 1
                 else count
      in (newPosition, newCount)
 
-movePart2 :: (Int, Int) -> (Char, Int) -> (Int, Int)
-movePart2 (position, count) (direction, amount) =
-    let newPosition =
-            if direction == 'L'
-                then (position - amount) `mod` 100
-                else (position + amount) `mod` 100
-        crossing = zeroCrossing position (direction, amount)
+movePart2 :: (Int, Int) -> (Direction, Int) -> (Int, Int)
+movePart2 (position, count) (L, amount) =
+    let newPosition = (position - amount) `mod` 100
+        crossing = zeroCrossing position L amount
+     in (newPosition, count + crossing)
+movePart2 (position, count) (R, amount) =
+    let newPosition = (position + amount) `mod` 100
+        crossing = zeroCrossing position R amount
      in (newPosition, count + crossing)
 
-zeroCrossing :: Int -> (Char, Int) -> Int
-zeroCrossing position (direction, amount)
-    | direction == 'R' = (position + amount) `div` 100
+zeroCrossing :: Int -> Direction -> Int -> Int
+zeroCrossing position R amount =
+    (position + amount) `div` 100
+zeroCrossing position L amount
     | position == 0 = amount `div` 100
     | amount < position = 0
     | otherwise = 1 + (amount - position) `div` 100
