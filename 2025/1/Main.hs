90a750ab228e88b6f7a0c6614c405304df1d173b
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-08-24 16:51:18 -0300

    feat: 2025-1-2

--- 2025/1/Main.hs
+++ 2025/1/Main.hs
@@ -3,18 +3,36 @@ main = do
     content <- readFile "input.txt"
     let instructions = filter (not . null) (lines content)
     let parsed = map parseInstruction instructions
-    let (finalPosition, zeroCount) = foldl move (50, 0) parsed
-    print zeroCount
+
+    let (_, part1) = foldl movePart1 (50, 0) parsed
+    let (_, part2) = foldl movePart2 (50, 0) parsed
+
+    print part1
+    print part2
 
 parseInstruction :: String -> Int
 parseInstruction ('L' : amount) = -(read amount)
 parseInstruction ('R' : amount) = read amount
 
-move :: (Int, Int) -> Int -> (Int, Int)
-move (position, count) amount =
+movePart1 :: (Int, Int) -> Int -> (Int, Int)
+movePart1 (position, count) amount =
     let newPosition = (position + amount) `mod` 100
         newCount =
             if newPosition == 0
                 then count + 1
                 else count
      in (newPosition, newCount)
+
+movePart2 :: (Int, Int) -> Int -> (Int, Int)
+movePart2 (position, count) amount =
+    foldl step (position, count) (replicate (abs amount) direction)
+  where
+    direction = if amount < 0 then -1 else 1
+
+    step (pos, count) dir =
+        let newPosition = (pos + dir) `mod` 100
+            newCount =
+                if newPosition == 0
+                    then count + 1
+                    else count
+         in (newPosition, newCount)
