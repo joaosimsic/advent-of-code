deba473aa38906af3a9a6c10387af0e9d27f138c
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-08-24 16:08:44 -0300

    feat: 2025-1-1

--- 2025/1/Main.hs
+++ 2025/1/Main.hs
@@ -1,5 +1,20 @@
 main :: IO ()
 main = do
-  content <- readFile "input.txt"
-  let instructions = lines content
-  print instructions
+    content <- readFile "input.txt"
+    let instructions = filter (not . null) (lines content)
+    let parsed = map parseInstruction instructions
+    let (finalPosition, zeroCount) = foldl move (50, 0) parsed
+    print zeroCount
+
+parseInstruction :: String -> Int
+parseInstruction ('L' : amount) = -(read amount)
+parseInstruction ('R' : amount) = read amount
+
+move :: (Int, Int) -> Int -> (Int, Int)
+move (position, count) amount =
+    let newPosition = (position + amount) `mod` 100
+        newCount =
+            if newPosition == 0
+                then count + 1
+                else count
+     in (newPosition, newCount)
