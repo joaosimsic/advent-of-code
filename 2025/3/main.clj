96f1b8bd8a17b26ed455b4c7369ee105dee15ec4
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-08-26 17:09:14 -0300

    feat: 2025-3-1

--- 2025/3/main.clj
+++ 2025/3/main.clj
@@ -1,3 +1,26 @@
-(def content (slurp "input.txt"))
+(ns main
+  (:require [clojure.string :as str]))
 
-(println content)
+(defn highest-joltage [bank]
+  (let [digits (map #(Character/digit % 10) bank)]
+    (loop [remaining (rest digits)
+           max-first (first digits)
+           best-pair -1]
+      (if (empty? remaining)
+        best-pair
+        (let [curr (first remaining)
+              candidate (+ (* max-first 10) curr)]
+          (recur (rest remaining)
+                 (max max-first curr)
+                 (max best-pair candidate)))))))
+
+(defn -main []
+  (let [total-joltage (->> (slurp "input.txt")
+                           (str/split-lines)
+                           (remove str/blank?)
+                           (map highest-joltage)
+                           (reduce +))]
+
+    (println "Total joltage: " total-joltage)))
+
+(-main)
