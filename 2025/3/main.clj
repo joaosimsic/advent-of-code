8d9ffebd9601422408d406f17f5e19a602089d96
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-08-26 17:36:39 -0300

    feat: 2025-3-2

--- 2025/3/main.clj
+++ 2025/3/main.clj
@@ -1,26 +1,38 @@
 (ns main
   (:require [clojure.string :as str]))
 
-(defn highest-joltage [bank]
-  (let [digits (map #(Character/digit % 10) bank)]
-    (loop [remaining (rest digits)
-           max-first (first digits)
-           best-pair -1]
-      (if (empty? remaining)
-        best-pair
-        (let [curr (first remaining)
-              candidate (+ (* max-first 10) curr)]
-          (recur (rest remaining)
-                 (max max-first curr)
-                 (max best-pair candidate)))))))
+(defn highest-joltage
+  ([bank] (highest-joltage bank 12))
+  ([bank k]
+   (let [digits (mapv #(Character/digit % 10) bank)
+         n (count digits)
+         to-drop (- n k)]
+     (if (<= n k)
+       (reduce #(+ (* %1 10) %2) 0 digits)
+       (let [[stack _]
+             (reduce (fn [[st drops] curr]
+                       (let [[new-st remaining-drops]
+                             (loop [s st
+                                    d drops]
+                               (if (and (seq s)
+                                        (pos? d)
+                                        (< (peek s) curr))
+                                 (recur (pop s) (dec d))
+                                 [s d]))]
+                         [(conj new-st curr) remaining-drops]))
+                     [[] to-drop]
+                     digits)]
+         (->> stack
+              (take k)
+              (reduce #(+ (* %1 10) %2) 0)))))))
 
 (defn -main []
   (let [total-joltage (->> (slurp "input.txt")
                            (str/split-lines)
                            (remove str/blank?)
-                           (map highest-joltage)
+                           (map #(highest-joltage % 12))
                            (reduce +))]
 
-    (println "Total joltage: " total-joltage)))
+    (println "Total joltage: " (str total-joltage))))
 
 (-main)
