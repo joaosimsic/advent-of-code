9856bab7c78f2ff0a7d5506860af8c01279d061b
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-08-27 18:04:56 -0300

    feat: vector of vectors

--- 2025/4/Main.hs
+++ 2025/4/Main.hs
@@ -1,7 +1,24 @@
 import qualified Data.Vector as V
 
+type Grid = V.Vector (V.Vector Char)
+
+parseGrid :: String -> Grid
+parseGrid s =
+    V.fromList
+        . map V.fromList
+        . filter (not . null)
+        $ lines s
+
+coordinates :: Grid -> [(Int, Int)]
+coordinates grid =
+    [ (row, col)
+    | row <- [0 .. V.length grid - 1]
+    , col <- [0 .. V.length (grid V.! 0) - 1]
+    ]
+
 main :: IO ()
 main = do
     content <- readFile "2025/4/input.txt"
-    let x = V.fromList $ filter (not . null) (lines content)
-    print x
+    let grid = parseGrid content
+    print $ V.length grid
+    print $ V.length (grid V.! 0)
